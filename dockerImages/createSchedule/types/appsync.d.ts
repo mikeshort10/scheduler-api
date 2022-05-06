75dbbc2bee704a9bfe8963ada84ce9dc92b4d77c
Mike Short <mikeshort2014@gmail.com>
2022-05-06 15:16:08 -0400

    refactor: remove dead code

--- dockerImages/createSchedule/types/appsync.d.ts
+++
@@ -1,4 +0,0 @@
-import type { AppSyncResolverHandler } from "aws-lambda";
-import type { GraphQLError } from "graphql";
-import type { Mutation, Query } from "./api";
-export declare type APSGraphQLAPIResolverHandler<FieldName extends keyof Query | keyof Mutation, Args> = AppSyncResolverHandler<Args, FieldName extends keyof Query ? Query[FieldName] : FieldName extends keyof Mutation ? Mutation[FieldName] : never | GraphQLError | void>;
